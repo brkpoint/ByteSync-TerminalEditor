a8105bbf8b3cef36e1104c90d8f1e8825856e004
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-02-05 16:43:27 +0100

    oops forgot to add a comment, added some stuff i guess

--- src/Terminal/TerminalEngine.cpp
+++ src/Terminal/TerminalEngine.cpp
@@ -6,26 +6,31 @@ static int lastkeycode;
 TerminalEngine::TerminalEngine() {
     //system("stty raw");
     inputManager = new TerminalInput();
+
     running = true;
-    keycode = 0;
+    keycode = -1;
+    lastkeycode = -1;
     exitcode = 0;
 }
 
 TerminalEngine::~TerminalEngine() {
-    running = NULL;
-    keycode = NULL;
-    lastkeycode = NULL;
-    exitcode = NULL;
+    
+    running = false;
+    keycode = -1;
+    lastkeycode = -1;
+    exitcode = 0;
 }
 
 int TerminalEngine::start() {
     inputManager->openInput();
     while(running) {
-        if (inputManager->isInputAvaiable()) keycode = inputManager->readInput();
-        if (keycode == 13) return exitcode = 0;
-        if (keycode != lastkeycode) {
-            cout << keycode;
-            lastkeycode = keycode;
+        if (inputManager->isInputAvaiable()) {
+            keycode = inputManager->readInput();
+            if (keycode == 13) return exitcode = 0;
+            if (keycode != lastkeycode) {
+                cout << keycode;
+                lastkeycode = keycode;
+            }
         }
         onUpdate();
 
